eafa80a62d6d82afa3170b218c6add656f02838d
Noemie Lachman <noemie.lachman@hotmail.fr>
2020-11-13 22:32:24 +0000

    initialize

---
+++ config/initializers/cloudinary.rb
@@ -0,0 +1,8 @@
+
+Cloudinary.config do |config|
+    config.cloud_name = 'dbb96qhmk'
+    config.api_key = '829868612275985'
+    config.api_secret = 'LxBPXzbr9P-pu9imUZ9G_MGRm1s'
+    config.secure = true
+    config.cdn_subdomain = true
+  end
